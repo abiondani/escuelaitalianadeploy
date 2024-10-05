50584f862875a3b70e2f6d96d6bc27dc35f64dda
Ale <abiondani@gmail.com>
2024-10-05 19:47:55 -0300

    modelo

--- models/escuela.js
+++ models/escuela.js
@@ -1,3 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const alumnoSchema = new Schema ({
+    nombre: {
+        type: String,
+        required: true
+    }
+});
+
+const Alumno = mongoose.model("Alumno", alumnoSchema);
+module.exports = Alumno;
