b1e6d88f503587ded8b886c6476fef6a6a3656c0
Ale <abiondani@gmail.com>
2024-10-13 17:07:07 -0300

    mensajes de error y redireccion

--- models/escuela.js
+++ models/escuela.js
@@ -1,12 +1,34 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const alumnoSchema = new Schema ({
+const alumnoSchema = new Schema({
     nombre: {
         type: String,
-        required: true
-    }
+        required: true,
+    },
+    apellido: {
+        type: String,
+        required: true,
+    },
+});
+
+const usuarioSchema = new mongoose.Schema({
+    usuario: {
+        type: String,
+        required: true,
+    },
+    clave: {
+        type: String,
+        required: true,
+    },
+    rol: {
+        type: String,
+        required: true,
+    },
 });
 
 const Alumno = mongoose.model("Alumno", alumnoSchema);
+const Usuario = mongoose.model("Usuario", usuarioSchema);
+
 module.exports = Alumno;
+module.exports = Usuario;
